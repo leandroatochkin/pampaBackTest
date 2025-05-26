87f065b7054cd9d032929a98b7b3936b22496ea0
leandroatochkin <leandro.atochkin@outlook.es>
2025-05-26 10:53:48 -0300

    update

--- api/routes/token/upload.js
+++ api/routes/token/upload.js
@@ -10,17 +10,20 @@ router.post('/', async (req, res, next) => {
       
         const item = parseFileData(req.body.data);
         console.log('item', item)
-    
-
-
-            const { SIMBOLO, NOMBRE, VALOR_COMPRA, VALOR_VENTA, STOCK, VENCIMIENTO } = item[0];
-
-            await db.query(`
+        if (!item || item.length === 0) {
+            return res.status(400).json({ message: 'No data provided or data is invalid' });
+        }
+        // Check if the token already exists
+        for (const data of item) {
+                const { SIMBOLO, NOMBRE, VALOR_COMPRA, VALOR_VENTA, STOCK, VENCIMIENTO } = data
+                await db.query(`
                 INSERT INTO pampaTokenVariations 
                 (SIMBOLO, NOMBRE, VALOR_COMPRA, VALOR_VENTA, STOCK, VENCIMIENTO) 
                 VALUES (?, ?, ?, ?, ?, ?)
             `,[SIMBOLO, NOMBRE, VALOR_COMPRA, VALOR_VENTA, STOCK, VENCIMIENTO])
+        }
 
+            
             res.status(201).json({ message: 'token updated successfully' });
             return;
     } catch (err) {
