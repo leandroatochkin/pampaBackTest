20b36e85279d6e0aa6a8dff21ecf294ad813e4ea
leandroatochkin <leandro.atochkin@outlook.es>
2025-06-19 15:06:15 -0300

    update

--- runQuery.js
+++ runQuery.js
@@ -2,7 +2,7 @@ import { db } from "./api/db/db.js";
 
 async function testConnection() {
   try {
-    await db.query('ALTER TABLE users MODIFY password VARCHAR(60) NOT NULL');
+    await db.query('ALTER TABLE users MODIFY country VARCHAR(3) NOT NULL');
     console.log('done');
   } catch (err) {
     console.error('❌ Connection failed:', err.message);
