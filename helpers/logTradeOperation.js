8566f7322357085529400ecc657d3cdc38f9effa
leandroatochkin <leandro.atochkin@outlook.es>
2025-06-19 11:51:51 -0300

    update

--- helpers/logTradeOperation.js
+++ helpers/logTradeOperation.js
@@ -1,6 +1,7 @@
 import { getOrCreateOperationFile } from "./createOperationsFile.js";
 import { appendTradeOperation } from "./appendTradeOperation.js";
 import { drive } from "../storage/drive.js";
+import dayjs from "dayjs";
 const folderId = process.env.GOOGLE_DRIVE_REGISTRY_FOLDER
 
 function formatOperationText(data) {
