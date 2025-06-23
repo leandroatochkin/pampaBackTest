8a3c3d26fca8f3c3ef7b55993f0e10d2ae672d5c
leandroatochkin <leandro.atochkin@outlook.es>
2025-06-23 12:55:32 -0300

    update

--- helpers/logTradeOperation.js
+++ helpers/logTradeOperation.js
@@ -5,7 +5,7 @@ import dayjs from "dayjs";
 const folderId = process.env.GOOGLE_DRIVE_REGISTRY_FOLDER
 
 function formatOperationText(data) {
-  return `[${data.CUIT}|${data.operationType ? 0 === 'C' : 'V' }|${(data.token).padEnd(30)}|${(data.amount).padEnd(10)}|${(Number(data.price/100).toFixed(2)).padEnd(10)|('100.50').padEnd(10)};`;//format the output to the file
+  return `[${data.CUIT}|${data.operationType ? 0 === 'C' : 'V' }|${(data.token).padEnd(30)}|${(String(data.amount)).padEnd(10)}|${(String(Number(data.price/100).toFixed(2))).padEnd(10)|('100.50').padEnd(10)};`;//format the output to the file
 }
 
 export async function logTradeOperation(operationData) {
