b170b5488c99825d4ade08b236334ba91b9e4087
leandroatochkin <leandro.atochkin@outlook.es>
2025-07-14 20:29:37 -0300

    update

--- storage/drive.js
+++ storage/drive.js
@@ -2,6 +2,8 @@ import { google } from "googleapis";
 
 const keyFilePath = '/etc/secrets/pampatokens-32095e817f7a.json';
 //const keyFilePath = '../pampabacktest/secrets/pampatokensstorage-d1e00b34af25.json';
+const key = JSON.parse(fs.readFileSync(keyFilePath, 'utf8'));
+console.log('✅ Using service account:', key.client_email);
 
 const auth = new google.auth.GoogleAuth({
   keyFile: keyFilePath,
@@ -9,5 +11,6 @@ const auth = new google.auth.GoogleAuth({
 });
 
 export const folderId = process.env.GOOGLE_DRIVE_FOLDER_ID
+console.log('📁 Uploading to folder ID:', folderId);
 
 export const drive = google.drive({ version: 'v3', auth });
