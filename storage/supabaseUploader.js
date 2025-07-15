8e4cb7031466f2cee3748ee338b8f16c77e8cb58
leandroatochkin <leandro.atochkin@outlook.es>
2025-07-15 14:03:44 -0300

    changes storage provider

--- storage/supabaseUploader.js
+++ storage/supabaseUploader.js
@@ -2,7 +2,7 @@
 import { createClient } from '@supabase/supabase-js';
 import fs from 'fs';
 
-const supabase = createClient(process.env.SUPABASE_BUCKET_URL, process.env.SUPABASE_ANON_KEY);
+const supabase = createClient(process.env.SUPABASE_BUCKET_URL, process.env.SUPABASE_ROLE_KEY);
 const bucket = process.env.SUPABASE_BUCKET_NAME;
 
 export const uploadToSupabase = async (filePath, destPath) => {
