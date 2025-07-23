300127faf8e3ebd61f42da6610f7567055742fb9
leandroatochkin <leandro.atochkin@outlook.es>
2025-07-22 23:01:59 -0300

    UPDATE

--- api/routes/token/buyToken.js
+++ api/routes/token/buyToken.js
@@ -3,12 +3,27 @@ import { db } from '../../db/db.js';
 import { authenticateToken } from '../../middleware/auth.js';
 import { v4 as uuidv4 } from 'uuid';
 import { logTradeOperation } from '../../../helpers/logTradeOperation.js';
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
+
+dayjs.extend(utc);
+dayjs.extend(timezone); 
+
 
 const router = express.Router();
 
 router.post('/', authenticateToken, async (req, res) => {
     const { userId, amount, symbol, boughtAtValue, tokenName } = req.body;
 
+    const nowInBuenosAires = dayjs().tz('America/Argentina/Buenos_Aires');
+    const hour = nowInBuenosAires.hour();
+    
+    if (hour >= 18) {
+        return res.status(403).json({ 
+            message: 'Mercado cerrado. No puede comprar luego de las 18:00hs (Buenos Aires time).' 
+        });
+    }
 
     if (!userId || amount == null || !symbol || !boughtAtValue) {
         return res.status(400).json({ message: 'Missing required fields' });
@@ -33,30 +48,24 @@ router.post('/', authenticateToken, async (req, res) => {
         );
 
         if (existingRows.length === 0) {
-            // Step 2a: Insert new token entry
             await connection.query(
                 `INSERT INTO userTokens (userId, tokenSymbol, tokenName, tokenAmount, tokenPaidPrice) VALUES (?, ?, ?, ?, ?)`,
                 [userId, symbol, tokenName, amount, boughtAtValue]
             );
-        
-        
-
         } else {
-            // Step 2b: Update existing token entry
             await connection.query(
                 `UPDATE userTokens SET tokenAmount = tokenAmount + ?, tokenPaidPrice = ? WHERE userId = ? AND tokenSymbol = ?`,
                 [amount, boughtAtValue, userId, symbol]
             );
         }
 
-        //start registry process
-
+        // Registry
         const getCUITresults = await connection.query(
             `SELECT CUIT FROM users WHERE id = ?`,
             [userId]
-        )
-        
-        const userCUIT = getCUITresults[0][0].CUIT
+        );
+
+        const userCUIT = getCUITresults[0][0].CUIT;
         const operationDTO = {
             CUIT: userCUIT,
             operationType: 0,
@@ -64,9 +73,9 @@ router.post('/', authenticateToken, async (req, res) => {
             tokenName: tokenName,
             amount: amount,
             price: boughtAtValue
-        }
+        };
 
-        await logTradeOperation(operationDTO)
+        await logTradeOperation(operationDTO);
 
         await connection.commit();
         res.status(200).json({ message: 'Operation successful' });
